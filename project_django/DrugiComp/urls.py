f2b721446e41900d93c1f842bb34711167af1f8a
Camile <60142899+Cam-eraaa@users.noreply.github.com>
2020-02-05 10:17:11 +0100

    Update urls.py

--- project_django/DrugiComp/urls.py
+++ project_django/DrugiComp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='DrugiComp-home'),
     path('about/', views.about, name='DrugiComp-about'),
+    path('statistics/', views.statistics, name='DrugiComp-statistics'),
 ]
