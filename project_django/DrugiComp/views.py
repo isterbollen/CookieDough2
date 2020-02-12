6e1e75283fc86e1cf6af1a1ff91e2c272d29cfbf
Camile <60142899+Cam-eraaa@users.noreply.github.com>
2020-02-12 13:47:09 +0100

    Update views.py

--- project_django/DrugiComp/views.py
+++ project_django/DrugiComp/views.py
@@ -2,37 +2,14 @@ from django.shortcuts import render
 from django.db import connection
 
 def home(request):
-	cur = connection.cursor()
-	drug1=request.POST.get("drug1")
-	drug2=request.POST.get("drug2")
-	listD=[drug1,drug2]
-	listS=[]
-	listI=[]
-	for i in listD:
-		cur.execute("SELECT subst_name FROM drug WHERE name=%s",([i]))
-		r=cur.fetchall()
-		for row in r:
-			listS.append(row[0])
-	cur.execute("SELECT * FROM interactions")
-	r=cur.fetchall()
-	for row in r:
-		li=list(row[0:])
-		if li[0] in listS and li[1] in listS:
-			listI.append(li)
-	context = {'interactions':listI}
-	return render(request, 'DrugiComp/home.html', context)
-
-def about(request):
-	return render(request, 'DrugiComp/about.html', {'title':'About'})
-
-def statistics(request):
 	cur=connection.cursor()
+	cur.execute("SELECT name FROM drug")
+	r=cur.fetchall()
+	ndrugs=[row[0] for row in r]
 	listD=[]
 	listDbis=[]
 	listS=[]
 	listI=[]
-	cur.execute("SELECT name FROM drug")
-	ndrugs=cur.fetchall()
 	for i in range(5):
 		if request.POST.get(f'drug{i}')!=None:
 			listD.append(request.POST.get(f'drug{i}'))
@@ -48,7 +25,14 @@ def statistics(request):
 		li=list(row[0:])
 		if li[0] in listS and li[1] in listS:
 			listI.append(li)
-	context={'title':'Statistics', 'listI':listI, 'listDbis':listDbis, 'drugs':ndrugs}
+	context={"nd":ndrugs, 'listI':listI, 'listDbis':listDbis}
+	return render(request, 'DrugiComp/home.html', context)
+
+def about(request):
+	return render(request, 'DrugiComp/about.html', {'title':'About'})
+
+def statistics(request):
+	context={'title':'Statistics'}
 	return render(request, 'DrugiComp/statistics.html', context)
 
 def test(request):
