88f3cac6f787d526ee4afc69868794beec7fd4c7
Camile <60142899+Cam-eraaa@users.noreply.github.com>
2020-02-20 11:50:51 +0100

    Update views.py

--- project_django/DrugiComp/views.py
+++ project_django/DrugiComp/views.py
@@ -2,28 +2,39 @@ from django.shortcuts import render
 from django.db import connection
 
 def home(request):
+	# Start by creating a list of all the drugs that there are in the database for the dropdown list
 	cur=connection.cursor()
 	cur.execute("SELECT name FROM drug")
 	r=cur.fetchall()
+	# We fetch the first item of each row (the name of the drug)
 	ndrugs=[row[0] for row in r]
 	listD=[]
 	listDbis=[]
 	listS=[]
 	listI=[]
+	# For each form input (drug 1 to drug 5) we fetch what is in it and put it in listD
 	for i in range(6):
 		if request.POST.get(f'drug{i}')!=None:
 			listD.append(request.POST.get(f'drug{i}'))
+	# For each drug in list D we select the corresponding substance and add it in listS and we also populate the listDbis
+	# so that we have the drug (here 'i') and its substance (here row[0)
 	for i in listD:
 		cur.execute("SELECT subst_name FROM drug WHERE name=%s",([i]))
 		r=cur.fetchall()
 		for row in r:
 			listS.append(row[0])
 			listDbis.append([i,row[0]])
+	# We start by selecting each interaction in the database and then for each line of the SQL query, we check if the
+	# substances we have in our listS are present in the interaction list. In other words if one of our substances is
+	# in position 0 or 1 of a row and that another substance of our list is in the corresponding other position, we have
+	# an interaction
+	# We add only the interactions that came out in the list called listI
 	cur.execute("SELECT * FROM interactions")
 	r=cur.fetchall()
 	for row in r:
 		li=list(row[0:])
 		if li[0] in listS and li[1] in listS:
+			# Each row of this list contains 'subst_a', 'subst_b', 'description' and 'level'
 			listI.append(li)
 	context={"nd":ndrugs, 'listI':listI, 'listDbis':listDbis}
 	return render(request, 'DrugiComp/home.html', context)
